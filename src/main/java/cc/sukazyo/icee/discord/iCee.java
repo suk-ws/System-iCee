be7a0e89a5b3f6fcad121f32c2790438bd1d86fb
Eyre_S <Sukazyo@outlook.com>
2020-05-10 23:59:35 +0800

    build 3 - 完善 Log 和 Proper 系统 - 添加 log 等级系统和文件 log - Proper 添加读取配置和初始化配置 - 同时修改部分内容   - iCee 主类添加app信息   - 修复 git 忽略掉 resources 中的默认配置文件问题   - 默认 gradle 编码调整为 utf-8

--- src/main/java/cc/sukazyo/icee/discord/iCee.java
+++ src/main/java/cc/sukazyo/icee/discord/iCee.java
@@ -2,6 +2,7 @@ package cc.sukazyo.icee.discord;
 
 import cc.sukazyo.icee.discord.event.CommandReturn;
 import cc.sukazyo.icee.discord.system.Proper;
+import cc.sukazyo.icee.discord.util.Log;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
 
@@ -9,13 +10,19 @@ import javax.security.auth.login.LoginException;
 
 public class iCee {
 	
+	public static final String APPID = "icee-dc";
+	public static final String VERSION = "0.1.0";
+	public static final int BUILD_VER = 3;
+	public static final boolean DEBUG_MODE = true;
+	
 	public static JDABuilder builder;
 	
 	public static void main (String[] args) {
 		
-		Proper.init();
+		Log.info("Starting iCee Discord");
 		
-		System.out.println(Proper.TOKEN);
+		Proper.load();
+		Log.init();
 		
 		builder = new JDABuilder(AccountType.BOT);
 		builder.setToken(Proper.TOKEN);
@@ -28,6 +35,8 @@ public class iCee {
 //		} catch (LoginException e) {
 //			System.err.println("Logining Failed.");
 //		}
+		
+		Log.info("System off");
 		
 	}
 	
--- src/main/java/cc/sukazyo/icee/discord/system/Proper.java
+++ src/main/java/cc/sukazyo/icee/discord/system/Proper.java
@@ -1,26 +1,34 @@
 package cc.sukazyo.icee.discord.system;
 
+import cc.sukazyo.icee.discord.iCee;
+import cc.sukazyo.icee.discord.util.Log;
+
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Properties;
 
 public class Proper {
 	
 	public static String TOKEN;
+	public static int logLevel = Log.DEBUG;
+	public static boolean logDebug;
 	
-	public static void init () {
+	static Properties ini = new Properties();
+	
+	public static void load () {
 		
 		File iniFile = new File("./properties.ini");
-		Properties ini = new Properties();
 		
 		try {
 			if (!iniFile.isFile()) {
-				iniFile.createNewFile();
-				ini.setProperty("token", "<Enter Properties>");
-				ini.store(new FileOutputStream(iniFile), "System iCee Default");
-				// TODO 添加默认初始化系统
+				Log.warn("检测到没有配置文件，请等待系统生成默认配置文件后填写配置信息");
+				init(iniFile);
+				System.exit(0);
 			}
 		} catch (IOException e) {
-			// TODO ERRORDUMP
+			System.err.println("[FATAL]Found unexcepted error while read properties.");
+			e.printStackTrace();
+			System.exit(1);
 		}
 		
 		try {
@@ -29,10 +37,52 @@ public class Proper {
 			
 			TOKEN = ini.getProperty("token");
 			
+			if (iCee.DEBUG_MODE) {
+				logLevel = Log.DEBUG;
+				Log.debug("=======================================================");
+				Log.debug("                   !!! Confirm !!!");
+				Log.debug("             !!! You are being DEBUG MODE !!!");
+				Log.debug("=======================================================");
+			} else {
+				switch (ini.getProperty("log.level")) {
+					case "DEBUG":
+						logLevel = Log.DEBUG;
+						break;
+					case "INFO":
+						logLevel = Log.INFO;
+						break;
+					case "WARN":
+						logLevel = Log.WARN;
+						break;
+					case "ERROR":
+						logLevel = Log.ERROR;
+						break;
+					default:
+						Log.error("Unsupported Log Level. Log level must be DEBUG/INFO/WARN/ERROR");
+				}
+			}
+			
 		} catch (Exception e) {
-			// TODO ERRORDUMP
+			Log.error("Found error while reading properties");
 		}
 		
+	}
+	
+	private static void init (File iniFile) throws IOException {
+		if (iniFile.createNewFile()) {
+			iniFile.delete();
+			FileOutputStream os = new FileOutputStream(iniFile);
+			Log.info("Start Copy Default Properties");
+			BufferedInputStream ins = new BufferedInputStream(Proper.class.getResourceAsStream("/properties.ini"));
+			byte[] buf = new byte[4096];
+			while ((ins.read(buf))!=-1) {
+				os.write(buf);
+			}
+			os.close(); ins.close();
+			Log.info("Summon default properties done.");
+		} else {
+			throw new IOException("Create properties.ini Error");
+		}
 	}
 	
 }
