3915126ad230372d769e57891276e23ffff058c5
Eyre_S <sukazyo@outlook.com>
2021-08-29 21:49:43 +0800

    build 52 | help 命令现在支持了子命令的显示帮助和列出，使中文的翻译和英文的翻译同步

--- src/main/java/cc/sukazyo/icee/system/command/CommandWithChild.java
+++ src/main/java/cc/sukazyo/icee/system/command/CommandWithChild.java
@@ -27,4 +27,8 @@ public abstract class CommandWithChild extends CommandContainer implements IComm
 		}
 	}
 	
+	public boolean isHelpShowCommandLists () {
+		return true;
+	}
+	
 }
--- src/main/java/cc/sukazyo/icee/system/command/core/CommandHelp.java
+++ src/main/java/cc/sukazyo/icee/system/command/core/CommandHelp.java
@@ -38,31 +38,55 @@ public class CommandHelp implements ICommand, ICommandHelped {
 			return;
 			
 		} else
-			if (args.length == 1) { // 单个命令帮助信息
+			if (args.length > 0) { // 单个命令帮助信息
 				
 				if (CommandManager.getRegisteredCommandsMap().containsKey(args[0])) {
 					ICommand target = CommandManager.getRegisteredCommandsMap().get(args[0]);
+					StringBuilder commandName = new StringBuilder(args[0]);
+					for (int i = 1; i < args.length; i++) {
+						if (target instanceof CommandWithChild) {
+							target = ((CommandWithChild)target).getCommands().get(args[i]);
+							commandName.append(' ').append(args[i]);
+						} else {
+							Log.logger.info(I18n.getText(
+									"core.command.help.command_help_page.non_child",
+									new Var("parent", commandName.toString()),
+									new Var("next", args[i])
+							));
+						}
+					}
+					Var commandNameVar = new Var("command", commandName.toString());
+					boolean isUnsupported = true;
 					if (target instanceof ICommandHelped) {
+						isUnsupported = false;
 						if (
 								((ICommandHelped)target).getHelp() != null &&
 								!"".equals(((ICommandHelped)target).getHelp())
 						) {
 							helper.append(I18n.getText(
 									"core.command.help.command_help_page.page",
-									new Var("command", args[0]),
+									commandNameVar,
 									new Var("command_help_page", ((ICommandHelped)target).getHelp())
 							)).append('\n');
 						} else {
-							Log.logger.info(I18n.getText(
+							helper.append(I18n.getText(
 									"core.command.help.command_help_page.nan",
-									new Var("command", args[0])
-							));
-							return;
+									commandNameVar
+							)).append('\n');
 						}
-					} else {
+					}
+					if (target instanceof CommandWithChild && ((CommandWithChild)target).isHelpShowCommandLists()) {
+						isUnsupported = false;
+						helper.append(I18n.getText("core.command.help.command_help_page.child_list_title", commandNameVar));
+						helper.append('\n');
+						((CommandWithChild)target).getCommands().forEach(
+								(name ,command) -> listCommandsToBuilder(name, command, helper)
+						);
+					}
+					if (isUnsupported) {
 						Log.logger.info(I18n.getText(
 								"core.command.help.command_help_page.unsupported",
-								new Var("command", args[0])
+								commandNameVar
 						));
 						return;
 					}
@@ -74,69 +98,13 @@ public class CommandHelp implements ICommand, ICommandHelped {
 					return;
 				}
 				
-			} else
-				if (args.length == 0) { // 命令列表
-					
-					helper.append(I18n.getText("core.command.help.command_list.title")).append('\n');
-					
-					CommandManager.getRegisteredCommandsMap().forEach((name, command) -> {
-						// 检查是否为带有别名的命令
-						boolean isAliasCommand = false;
-						boolean isAvailableCommand = true;
-						if (command instanceof ICommandWithAlias) {
-							if (name.equals(((ICommandWithAlias)command).getName()))
-								isAliasCommand = true;
-							else
-								isAvailableCommand = false;
-						}
-						if (isAvailableCommand) {
-							// 输出命令名
-							helper.append(command.getType().getPrefix()).append(' '); // 命令名前缀，用于指示命令类型
-							if (isAliasCommand)
-								helper.append(((ICommandWithAlias)command).getName());
-							else
-								if (command.getRegistryName().size() == 1)
-									helper.append(command.getRegistryName().get(0));
-								else
-									helper.append(command.getRegistryName().toString());
-							// 输出命令调用规则和命令介绍
-							if (command instanceof ICommandHelped) {
-								if (
-										((ICommandHelped)command).getGrammar() != null &&
-										!"".equals(((ICommandHelped)command).getGrammar())
-								)
-									helper.append(' ').append(((ICommandHelped)command).getGrammar());
-								helper.append(" - ");
-								if (
-										((ICommandHelped)command).getIntroduction() == null ||
-										"".equals(((ICommandHelped)command).getIntroduction())
-								)
-									helper.append("N/A");
-								else
-									helper.append(((ICommandHelped)command).getIntroduction());
-							} else {
-								helper.append(" - ").append(I18n.getText("core.command.help.unsupported"));
-							}
-							helper.append('\n');
-							// 输出命令的别名
-							if (isAliasCommand) {
-								helper.append('\t').append(CommandType.getPrefixOfAlias())
-										.append(I18n.getText("core.command.help.command_list.single.alias_title"));
-								((ICommandWithAlias)command).getAvailableAliases()
-										.forEach(regNames -> helper.append(regNames).append(", "));
-								helper.delete(helper.length() - 2, helper.length()).append(".\n");
-							}
-						}
-					});
-					
-				} else { // 参数提供过多
-					try {
-						throw new CommandException.CommandNotFoundException(args[0]);
-					} catch (CommandException.CommandNotFoundException e) {
-						Log.logger.warn(I18n.getText("core.command.help.too_much_argument"));
-					}
-					return;
-				}
+			} else { // 命令列表
+				
+				helper.append(I18n.getText("core.command.help.command_list.title")).append('\n');
+				
+				CommandManager.getRegisteredCommandsMap().forEach((name, command) -> listCommandsToBuilder(name, command, helper));
+				
+			}
 		
 		Log.logger.info(helper.deleteCharAt(helper.length() - 1));
 		
@@ -155,6 +123,56 @@ public class CommandHelp implements ICommand, ICommandHelped {
 	@Override
 	public String getHelp () {
 		return I18n.getText("core.command.help.help");
+	}
+	
+	private static void listCommandsToBuilder (String name, ICommand command, StringBuilder echo) {
+		// 检查是否为带有别名的命令
+		boolean isAliasCommand = false;
+		boolean isAvailableCommand = true;
+		if (command instanceof ICommandWithAlias) {
+			if (name.equals(((ICommandWithAlias)command).getName()))
+				isAliasCommand = true;
+			else
+				isAvailableCommand = false;
+		}
+		if (isAvailableCommand) {
+			// 输出命令名
+			echo.append(command.getType().getPrefix()).append(' '); // 命令名前缀，用于指示命令类型
+			if (isAliasCommand)
+				echo.append(((ICommandWithAlias)command).getName());
+			else
+				if (command.getRegistryName().size() == 1)
+					echo.append(command.getRegistryName().get(0));
+				else
+					echo.append(command.getRegistryName().toString());
+			// 输出命令调用规则和命令介绍
+			if (command instanceof ICommandHelped) {
+				if (
+						((ICommandHelped)command).getGrammar() != null &&
+						!"".equals(((ICommandHelped)command).getGrammar())
+				)
+					echo.append(' ').append(((ICommandHelped)command).getGrammar());
+				echo.append(" - ");
+				if (
+						((ICommandHelped)command).getIntroduction() == null ||
+						"".equals(((ICommandHelped)command).getIntroduction())
+				)
+					echo.append("N/A");
+				else
+					echo.append(((ICommandHelped)command).getIntroduction());
+			} else {
+				echo.append(" - ").append(I18n.getText("core.command.help.unsupported"));
+			}
+			echo.append('\n');
+			// 输出命令的别名
+			if (isAliasCommand) {
+				echo.append('\t').append(CommandType.getPrefixOfAlias())
+						.append(I18n.getText("core.command.help.command_list.single.alias_title"));
+				((ICommandWithAlias)command).getAvailableAliases()
+						.forEach(regNames -> echo.append(regNames).append(", "));
+				echo.delete(echo.length() - 2, echo.length()).append(".\n");
+			}
+		}
 	}
 	
 }
